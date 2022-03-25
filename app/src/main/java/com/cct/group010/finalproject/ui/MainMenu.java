d6b2c9e6b0771ab92a5a2ed3caeb8c16913eb5cf
marcusvdemoura <marcusdemoura@outlook.com>
2022-03-25 13:07:59 +0000

    Partial creation of the main menu activity.

--- app/src/main/java/com/cct/group010/finalproject/ui/MainMenu.java
+++ app/src/main/java/com/cct/group010/finalproject/ui/MainMenu.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.cct.group010.finalproject.R;
 import com.cct.group010.finalproject.domain.Reservation;
 
 public class MainMenu extends AppCompatActivity {
 
-    private Button button;
+    private TextView reservationMoreInfo, reservationStatus,reservationDates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +22,37 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
 
-        button = findViewById(R.id.button);
+        reservationMoreInfo = (TextView) findViewById(R.id.reservationMoreInfo);
+
+        reservationStatus = (TextView) findViewById(R.id.reservationStatus);
+        reservationDates = (TextView) findViewById(R.id.reservationDates);
+        reservationStatus.setText(ImportantObjects.guest.getNextReservation().getReservationStatus());
+
+        String date = ImportantObjects.guest.getNextReservation().getCheckin().toString() + " -\n "
+                + ImportantObjects.guest.getNextReservation().getCheckout().toString();
+        reservationDates.setText(date);
 
 
-        button.setOnClickListener(view -> {
+
+
+
+        reservationMoreInfo.setOnClickListener(view -> {
 
             System.out.println(ImportantObjects.guest.toString());
+
             System.out.println(ImportantObjects.guest.getReservationList().get(0).toString());
+
+            System.out.println("THIS IS THE DATA I WANT TO PARSE inside de clickable:");
+            System.out.println(ImportantObjects.guest.toString());
+
+
+
 
 
         });
+
+
+
+
     }
 }
