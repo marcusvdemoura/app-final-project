d95111a627c7003e11e095daa00e6099ee1c2740
ggenelhoud <ggenelhoud@gmail.com>
2022-04-13 23:41:36 +0100

    Creating activity_reset_password screen

---
+++ app/src/main/java/com/cct/group010/finalproject/ResetPassword.java
@@ -0,0 +1,14 @@
+package com.cct.group010.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
